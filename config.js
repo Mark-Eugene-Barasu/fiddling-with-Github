fd86613d64335c121eae71ae2b48eb854ffb90e4
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2024-04-30 18:39:25 +0200

    Version 1

---
+++ config.js
@@ -0,0 +1,15 @@
+console.log("index.js");
+console.log("version1");
+
+
+for(let i = 0; i < 100; i++){document.write(`<p>this is what we used to do back in 2018</p> `)};
+
+(function background_coloring() {
+    let bg_color = document.querySelector("body");
+
+    bg_color.style.textAlign = "center";
+    bg_color.style.color = "gold";
+    bg_color.style.backgroundColor = "rgb(255, 0, 0)";
+    bg_color.style.fontWeight = 700;
+})(); // this is an IIFE
+
